2fc12bb8fad319505db2a144b9c7e07185733ba6
Ben Myers <benben28405678@gmail.com>
2019-08-01 09:55:29 -0700

    Initial Commit.

---
+++ App.js
@@ -0,0 +1,129 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.title}>THE MUSE</Text>
+              <Text style={styles.subtitle}>DREYFOOS SCHOOL OF THE ARTS</Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Latest</Text>
+
+              <View style={styles.articleContainer}>
+                <Image source={pic} style={{width: 50, height: 50}}/>
+                  <View style={styles.articleSubContainer}>
+                    <Text style={styles.articleTitle}>Test article Title</Text>
+                    <Text style={styles.articlePreview}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
+                    </Text>
+                  </View>
+              </View>
+
+            </View>
+          </View>
+          <View style={styles.footer}/>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const pic = {
+  uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  articleContainer: {
+    marginTop: 8,
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 0,
+  },
+  articleSubContainer: {
+    padding: 2,
+    marginLeft: 5,
+    flex: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: Colors.gray,
+    textAlign: 'center',
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: Colors.black,
+  },
+  articleTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  articlePreview: {
+    marginTop: 2,
+    fontSize: 14,
+    fontWeight: '300',
+    color: Colors.dark,
+  },
+  footer: {
+    backgroundColor: Colors.white,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 40,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
